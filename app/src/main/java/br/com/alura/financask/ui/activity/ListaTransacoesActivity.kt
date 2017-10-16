be77d330f502d7ecafc4e0bf4b13d70c26799906
alexfelipe <alexfelipevieira@gmail.com>
2017-10-16 10:01:55 -0200

    video 1.4

--- app/src/main/java/br/com/alura/financask/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/br/com/alura/financask/ui/activity/ListaTransacoesActivity.kt
@@ -30,7 +30,7 @@ class ListaTransacoesActivity : AppCompatActivity() {
         val resumoView = ResumoView(view, transacoes)
         resumoView.adicionaReceita()
         resumoView.adicionaDespesa()
-
+        resumoView.adicionaTotal()
     }
 
     private fun configuraLista(transacoes: List<Transacao>) {
