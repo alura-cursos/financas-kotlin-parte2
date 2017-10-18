b15231c43a05c07031c57f9d85e5d2b266a9063e
alexfelipe <alexfelipevieira@gmail.com>
2017-10-18 10:03:00 -0200

    video 4.2

--- app/src/main/java/br/com/alura/financask/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/br/com/alura/financask/ui/activity/ListaTransacoesActivity.kt
@@ -1,8 +1,11 @@
 package br.com.alura.financask.ui.activity
 
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
 import br.com.alura.financask.R
 import br.com.alura.financask.model.Tipo
@@ -28,8 +31,16 @@ class ListaTransacoesActivity : AppCompatActivity() {
 
         lista_transacoes_adiciona_receita
                 .setOnClickListener {
-                    Toast.makeText(this,
-                            "clique de receita", Toast.LENGTH_LONG).show()
+                    val view: View = window.decorView
+                    val viewCriada = LayoutInflater.from(this)
+                            .inflate(R.layout.form_transacao,
+                                    view as ViewGroup,
+                                    false)
+
+                    AlertDialog.Builder(this)
+                            .setTitle(R.string.adiciona_receita)
+                            .setView(viewCriada)
+                            .show()
                 }
 
 
